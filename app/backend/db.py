27ca8677fa9e945fb7ecdda225a4dfc14815f4d5
rva174 <rva174@yandex.ru>
2024-12-31 17:26:11 +0500

    init

---
+++ app/backend/db.py
@@ -0,0 +1,13 @@
+# В модуле db.py:
+# Импортируйте все необходимые функции и классы , создайте движок указав пусть в БД
+# 'sqlite:///taskmanager.db' и локальную сессию (по аналогии с видео лекцией).
+# Создайте базовый класс Base для других моделей, наследуясь от DeclarativeBase.
+
+from sglalchemy import create_engine
+from sglalchemy.orm import sessionmaker, DeclarativeBase
+from sglalchemy import Column, Integer, String
+
+engine = create_engine("sqlite:///taskmanager.db")
+
+class Base(DeclarativeBase):
+    pass
