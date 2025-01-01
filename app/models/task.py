f05ca9a04edcb57555afc62ea018086507956a24
rva174 <rva174@yandex.ru>
2025-01-01 14:41:02 +0500

    init

--- app/models/task.py
+++ app/models/task.py
@@ -10,3 +10,28 @@
 # # user - объект связи с таблицей с таблицей User, где back_populates='tasks'.
 
 
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Float
+from sqlalchemy.orm import relationship
+from sqlalchemy.schema import CreateTable
+
+from app.models.user import User
+from app.backend.db import Base
+
+
+class Task(Base):
+    __tablename__ = 'tasks'
+    __table_args__ = {'keep_existing'}
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    priority = Column(Integer)
+    priority = Column(Integer, default=0)
+    completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey('user_id'))
+    slug = Column(String, unique=True, index=True)
+
+    is_active = Column(Boolean, default=True)
+
+    user = relationship('User', back_populates='tasks')
+
+print(CreateTable(Task.__table__))
